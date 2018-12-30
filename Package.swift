28558c11e83b56728fc673120163777e26ec79b3
Ryan <ryan@buckettechnologies.com>
2018-12-30 15:43:08 -0500

    Updating the package swift file to use my packeges

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
             targets: ["PerfectRequestLogger"]),
     ],
     dependencies: [
-			.package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", from: "3.0.0"),
+			.package(url: "https://github.com/ryancoyne/Perfect-Logger.git", from: "4.0.0"),
     ],
     targets: [
         .target(
